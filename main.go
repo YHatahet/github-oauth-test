2c8cd78c62a2c91d8e5b8944ff8ffe7d6d954682
YHatahet <yamen.hatahet@gmail.com>
2024-12-04 13:12:56 +0400

    Added main with routes

--- main.go
+++ main.go
@@ -17,3 +17,26 @@ func init() {
 		log.Fatal("No .env file found")
 	}
 }
+
+func main() {
+
+	// Simply returns a link to the login route
+	http.HandleFunc("/", rootHandler)
+
+	// Login route
+	http.HandleFunc("/login/github/", githubLoginHandler)
+
+	// Github callback
+	http.HandleFunc("/login/github/callback", githubCallbackHandler)
+
+	// Route where the authenticated user is redirected to
+	http.HandleFunc("/loggedin", func(w http.ResponseWriter, r *http.Request) {
+		loggedinHandler(w, r, "")
+	})
+
+	fmt.Println("[ UP ON PORT 3000 ]")
+	log.Panic(
+		http.ListenAndServe(":3000", nil),
+	)
+}
+
