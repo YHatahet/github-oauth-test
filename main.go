d6efd491a92e3677b0150d8905ac56672b7342bf
YHatahet <yamen.hatahet@gmail.com>
2024-12-04 13:12:44 +0400

    Added main file with init & imports

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("No .env file found")
+	}
+}
